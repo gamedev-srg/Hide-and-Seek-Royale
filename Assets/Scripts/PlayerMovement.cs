8c35cbc12f3916ffa0908be8290da6df3828cd2b
gidon285 <gidoanvziz1@gmail.com>
2022-01-03 15:14:21 +0200

    added movment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,7 +29,14 @@ public class PlayerMovement : MonoBehaviour
         //controller for momvement
         characterController = GetComponent<CharacterController>();
     }
-
+    void Update(){
+            Vector3 temp = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+            temp.Normalize();
+            float velocityZ = Vector3.Dot(temp.normalized, transform.forward);
+            float velocityX = Vector3.Dot(temp.normalized, transform.right);
+            animator.SetFloat("velocityZ", velocityZ,0.1f,Time.deltaTime);
+            animator.SetFloat("velocityX", velocityX,0.1f,Time.deltaTime);
+    }
     void FixedUpdate()
     {
         //only move if you're not dead.
@@ -38,7 +45,14 @@ public class PlayerMovement : MonoBehaviour
             var xAxisMouse = Input.GetAxis("Mouse X");
             var zAxis = Input.GetAxis("Vertical");
             var xShuffle = Input.GetAxis("Horizontal");
-            var movmentVector = new Vector3(xAxisMouse, 0, zAxis);
+            var movmentVector = new Vector3(0f,0f,0f);
+            if( xAxisMouse != 0){
+                movmentVector = new Vector3(xAxisMouse, 0, zAxis);
+            }else if( xShuffle != 0 ){
+                movmentVector = new Vector3(xShuffle,0,zAxis);
+            }else if( zAxis != 0){
+                movmentVector = new Vector3(0,0,zAxis);
+            }
             animator.SetFloat("speed", movmentVector.magnitude);
             //used for AD movment but it didn't work well.
             characterController.SimpleMove(xShuffle * transform.right * speedToUse);
