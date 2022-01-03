06a5676d858b8c9cfcf3c7a462f8d78eabe57f8d
gidon285 <gidoanvziz1@gmail.com>
2022-01-03 16:04:20 +0200

    Added text to Tutorial

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -19,13 +19,16 @@ public class Dialog : MonoBehaviour
     }
     void initlines(){
         textspeed = 0.02f;
-        lines = new string[6];
+        lines = new string[9];
         lines[0]= "Hello and welcome to Hide and Seek Royal! To get to the next dialog please press the Enter key.";
-        lines[1] ="Dialog can be also skipped during the writing by clicking the left mouse bottun, but we hope you read it all.";
+        lines[1] ="Dialog can be also skipped during the writing by clicking Enter, but we hope you read it all.";
         lines[2] = "The goal in HNSR is to find and shoot the hiders, but be carefull, they will escape you.";
         lines[3] = "To move you charecter use W,A,S,D keys. You point with your mouse and can also navigate with it.";
-        lines[4] = "Please move past the trucks to start the Demo.";
-        lines[5] ="See that guy? try to shot him!";
+        lines[4] = "To place a trap click the F key, you have two traps at the start, and there are more to find.";
+        lines[5] = "But be carful! your trap can hurt you aswell as the enemies.";
+        lines[6] = "Notice you have a limited amount of ammo, that also can be found around the map.";
+        lines[7] = "Please move past the trucks to start the Tutorial.";
+        lines[8] ="See that guy? try to shot him!";
 
     }
     // Update is called once per frame
