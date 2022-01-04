7451c5425259429cbea56a4759d93d3bf43942c9
gidon285 <gidoanvziz1@gmail.com>
2022-01-04 20:50:04 +0200

    finale

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    private Scene m_Scene;
+    private string sceneName;
+    private void Start(){
+        m_Scene = SceneManager.GetActiveScene();
+        sceneName = m_Scene.name;
+    }
+    public void Update(){
+        if(sceneName == "eDemo" || sceneName == "Win"){
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
     public void Play(){
         SceneManager.LoadScene("Scene1");
     }
